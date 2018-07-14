845e70f2201dd31a16405ff05f60024ceabe2d3b
Albert Cieslak <albert.cieslak2@gmail.com>
2018-07-14 17:55:39 +0200

    client

--- src/client/client.java
+++ src/client/client.java
@@ -23,7 +23,7 @@ public class client {
         System.out.flush();
     }
     private static void exit() throws IOException {
-        printFileContents("instruction");
+        printFileContents("resources/instruction");
         System.exit(1);
     }
     public static void main(String[] args) throws IOException {
@@ -33,18 +33,25 @@ public class client {
         try {
             if (args.length == 0) {
                 exit();
-            } else if (args[0] == "-s") {
+            } else if (args[0].equals("-s") ) {
                 List<Integer> days = new ArrayList<Integer>();
                 for (int i = 1; i < args.length; i++) {
                     days.add(Integer.parseInt(args[i]));
-
-                    Holder<WeatherSummary> result = new Holder<WeatherSummary>();
-
-                    port.compareForecast(new ManyDays(days),result);
-
-
-
                 }
+
+                Holder<WeatherSummary> result = new Holder<WeatherSummary>();
+
+                port.compareForecast(new ManyDays(days),result);
+
+                for(DailyWeatherSummary dailyWeather : result.value.dailyWeather) {
+                    System.out.println("Prognoza pogody dla dnia " + dailyWeather.day);
+                    System.out.println("Maksymalna(uśredniona) temperatura: " + dailyWeather.avgMaxTemperature);
+                    System.out.println("Minimalna(uśredniona) temperatura: " + dailyWeather.avgMinTemperature);
+                    System.out.println("Różnica w maksymalnej temperaturze podawanej przez serwisy " + dailyWeather.maxAmplitude);
+                    System.out.println("Różnica w minimalnej temperaturze podawanej przez serwisy " + dailyWeather.minAmplitude);
+                    System.out.println("");
+                }
+
             } else {
                 exit();
             }
